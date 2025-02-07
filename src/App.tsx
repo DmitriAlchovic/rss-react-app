29521dbcb8fd6d4e3fcfede3b49de097ac9322ef
vboxuser <alchovicdmitry@gmail.com>
2025-02-07 15:18:58 +0000

    feat: add router, layout, Error page

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,25 @@
 import { FC } from 'react';
 import ErrorBoundary from './components/ErrorBoundary';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router';
+import Layout from './components/Layout';
 import Home from './pages/Home/Home';
+import Error from './pages/Error/Error';
 
 const App: FC = () => {
   return (
     <>
-      <ErrorBoundary>
-        <Home />
-      </ErrorBoundary>
+      <BrowserRouter>
+        <ErrorBoundary>
+          <Routes>
+            <Route path="/" element={<Layout />}>
+              <Route index element={<Home />} />
+              <Route path="*" element={<Error />} />
+              <Route path="monster/:id" element={<Home />} />
+            </Route>
+          </Routes>
+        </ErrorBoundary>
+      </BrowserRouter>
     </>
   );
 };
---
+++ src/pages/Error/Error.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react';
+
+const Error: FC = () => {
+  return (
+    <>
+      <h1>404 NOT FOUND</h1>
+    </>
+  );
+};
+
+export default Error;
